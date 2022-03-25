b2310dcaf07c0825fc6b666c3e9a492c30192da2
Slobodan Milanko <support@justprogress.net>
2022-03-25 14:10:34 -0400

    added updates to the downloader

--- downloader.r
+++ downloader.r
@@ -18,10 +18,23 @@ fetchManuallyStoredInmateInfo <- function(execution_number) {
 	return(read.properties(paste("manual_inmate_info/",execution_number,".properties", sep=""), fields = NULL, encoding = "UTF-8"))
 }
 
-tryToPrefillValueForTessaractKey <- function(text, key, expectedLoc) {
+removeFuncCharactersFromString <- function(data) {
+	return(gsub("[\"() ~*?°_]", "", data))
+}
+
+tryToPrefillValueForTessaractKey <- function(text, key, expectedLoc, isDate) {
 	matchIdx = match(1, str_detect(regex(gsub("[\"() ~*\\?°_]", "", text$word), ignore_case = TRUE), paste("^", key, sep="")))
 	if (is.na(matchIdx) ) ( return("n/a"))
-	return(gsub("[\"() ~*?°_]", "", text$word[matchIdx + expectedLoc]))
+	sanatizedString = removeFuncCharactersFromString(text$word[matchIdx + expectedLoc])
+	if (length(sanatizedString) > 3 || isFALSE(isDate)) { return(sanatizedString) }
+	# let's try to build the date, as it's chunked out
+	startIdx = if (expectedLoc < 0) -2 else 1
+	parsedDate = ''
+	for (i in 1:2) {
+		parsedDate = paste(parsedDate, removeFuncCharactersFromString(text$word[matchIdx + (expectedLoc + startIdx)]), sep="")
+		startIdx = startIdx + 1
+	}
+	return(str_extract(if (expectedLoc < 0) paste(parsedDate, sanatizedString, sep="") else paste(sanatizedString, parsedDate, sep=""), '[0-9]{2}/[0-9]{2}/[0-9]{2}'))
 }
 
 downloadInmateData <- function() {
@@ -127,16 +140,16 @@ downloadInmateData <- function() {
 			text <- tesseract::ocr_data(inmate_info_links[i], engine = tesseract("eng"))
 
 			form <- list(
-				"DateOfBirth:TXT" = tryToPrefillValueForTessaractKey(text, "Received", -1),
-				"DateReceived:TXT" = tryToPrefillValueForTessaractKey(text, "Received", 1),
-				"DateOfOffense:TXT" = tryToPrefillValueForTessaractKey(text, "Date", 3),
-				"Occupation:TXT" = tryToPrefillValueForTessaractKey(text, "Occupation", 1),
-				"EyeColor:TXT" = tryToPrefillValueForTessaractKey(text, "Eyes", 1),
+				"DateOfBirth:TXT" = tryToPrefillValueForTessaractKey(text, "Received", -1, TRUE),
+				"DateReceived:TXT" = tryToPrefillValueForTessaractKey(text, "Received", 1, TRUE),
+				"DateOfOffense:TXT" = tryToPrefillValueForTessaractKey(text, "Date", 3, TRUE),
+				"Occupation:TXT" = tryToPrefillValueForTessaractKey(text, "Occupation", 1, FALSE),
+				"EyeColor:TXT" = tryToPrefillValueForTessaractKey(text, "Eyes", 1, FALSE),
 				"Gender:TEXT" = "male",
-				"HairColor:TXT" = tryToPrefillValueForTessaractKey(text, "Hair", 1),
-				"NativeCounty:TXT" = tryToPrefillValueForTessaractKey(text, "Native", 2),
-				"NativeState:TXT" = tryToPrefillValueForTessaractKey(text, "State", 1),
-				"EducationLevel:TXT" = paste(tryToPrefillValueForTessaractKey(text, "Education", 2), "years", sep=" ")
+				"HairColor:TXT" = tryToPrefillValueForTessaractKey(text, "Hair", 1, FALSE),
+				"NativeCounty:TXT" = tryToPrefillValueForTessaractKey(text, "Native", 2, FALSE),
+				"NativeState:TXT" = tryToPrefillValueForTessaractKey(text, "State", 1, FALSE),
+				"EducationLevel:TXT" = paste(tryToPrefillValueForTessaractKey(text, "Education", 2, FALSE), "years", sep=" ")
 			)
 
 			correctedData = dlg_form(form, "Is this data correct?")$res
