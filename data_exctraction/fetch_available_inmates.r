603fabce7520d338a2eb23eec1d93c8334c3f452
Slobodan Milanko <support@justprogress.net>
2022-03-28 10:40:13 -0400

    fix output message during fetch call

--- data_exctraction/fetch_available_inmates.r
+++ data_exctraction/fetch_available_inmates.r
@@ -3,14 +3,13 @@ library("stringr")
 library("properties")
 
 manuallyStoreInmateInfo <- function(execution_number, info_link, last_statement_link, last_name, first_name, tdjc_number, age_at_execution, execution_date, race, offense_county) {
-	print(paste("storing found inmate", execution_number))
 	write.properties(file = paste("available_executions/",execution_number,".properties", sep=""), properties = list(infoLink = info_link, lastStatementLink = last_statement_link, lastName = last_name, firstName = first_name, tdjcNumber = tdjc_number, ageAtExecution = age_at_execution, executionDate = execution_date, race = race, offenseCounty = offense_county), fields = c("infoLink", "lastStatementLink", "lastName", "firstName", "tdjcNumber", "ageAtExecution", "executionDate", "race", "offenseCounty"))
 }
 
 fetchAvailableInmates <- function() {
 	# constants
 	baseUrl = "https://www.tdcj.texas.gov/death_row/"
-
+	print(paste("fetching available inmates from", baseUrl))
 	# read the inmate table
 	baseDoc <- read_html(paste(baseUrl, "dr_executed_offenders.html", sep=""))
 	allInmatesTable = baseDoc %>% html_nodes("table") %>% html_nodes("tr")
