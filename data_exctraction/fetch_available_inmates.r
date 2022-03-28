d7e420aa9cc338defb2908e0f312cadb75577862
Slobodan Milanko <support@justprogress.net>
2022-03-28 10:43:51 -0400

    rename function to be more appropriate

--- data_exctraction/fetch_available_inmates.r
+++ data_exctraction/fetch_available_inmates.r
@@ -2,7 +2,7 @@ library("rvest")
 library("stringr")
 library("properties")
 
-manuallyStoreInmateInfo <- function(execution_number, info_link, last_statement_link, last_name, first_name, tdjc_number, age_at_execution, execution_date, race, offense_county) {
+storeExecution <- function(execution_number, info_link, last_statement_link, last_name, first_name, tdjc_number, age_at_execution, execution_date, race, offense_county) {
 	write.properties(file = paste("available_executions/",execution_number,".properties", sep=""), properties = list(infoLink = info_link, lastStatementLink = last_statement_link, lastName = last_name, firstName = first_name, tdjcNumber = tdjc_number, ageAtExecution = age_at_execution, executionDate = execution_date, race = race, offenseCounty = offense_county), fields = c("infoLink", "lastStatementLink", "lastName", "firstName", "tdjcNumber", "ageAtExecution", "executionDate", "race", "offenseCounty"))
 }
 
@@ -17,7 +17,7 @@ fetchAvailableInmates <- function() {
 	# get the general data from the death row table
 	for(i in 2:length(allInmatesTable)) {
 		inmate = allInmatesTable[i] %>% html_nodes("td")
-		manuallyStoreInmateInfo(strtoi(inmate[1] %>% html_text()), paste(baseUrl, str_remove(inmate[2] %>% html_nodes("a") %>% html_attr('href'), "/death_row/"), sep=""), paste(baseUrl, str_remove(inmate[3] %>% html_nodes("a") %>% html_attr('href'), "/death_row/"), sep=""), inmate[4] %>% html_text(), inmate[5] %>% html_text(), strtoi(inmate[6] %>% html_text()), strtoi(inmate[7] %>% html_text()), inmate[8] %>% html_text(), inmate[9] %>% html_text(), inmate[10] %>% html_text())
+		storeExecution(strtoi(inmate[1] %>% html_text()), paste(baseUrl, str_remove(inmate[2] %>% html_nodes("a") %>% html_attr('href'), "/death_row/"), sep=""), paste(baseUrl, str_remove(inmate[3] %>% html_nodes("a") %>% html_attr('href'), "/death_row/"), sep=""), inmate[4] %>% html_text(), inmate[5] %>% html_text(), strtoi(inmate[6] %>% html_text()), strtoi(inmate[7] %>% html_text()), inmate[8] %>% html_text(), inmate[9] %>% html_text(), inmate[10] %>% html_text())
 	}
 	print(paste("great news, we have found", length(allInmatesTable) - 1, "inmates."))
 
