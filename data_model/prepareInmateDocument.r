e0ea3b8d033dd0d66b9fd0a9305dae59e1d7bfac
Slobodan Milanko <support@justprogress.net>
2022-03-29 23:32:40 -0400

    added script to generate the inmate data;

--- data_model/prepareInmateDocument.r
+++ data_model/prepareInmateDocument.r
@@ -8,6 +8,7 @@ prepareInmateDocument <- function() {
 	executions = list.files(path="../data_extraction/inmate_executions/", pattern=NULL, all.files=FALSE, full.names=FALSE)
 	print(paste("parsing through", length(executions), " inmate files 🥳"))
 
+	inmate_execution_number = c()
 	inmate_first_names = c()
 	inmate_last_names = c()
 	inmate_execution_dates = c()
@@ -25,7 +26,6 @@ prepareInmateDocument <- function() {
 	inmate_last_statements = c()
 
 	for (i in 1:length(executions)) {
-
 		downloadedExecution = read.properties(paste("../data_extraction/inmate_executions/", executions[i], sep=""), fields = NULL, encoding = "UTF-8")
 		inmateInfo = read.properties(paste("../data_extraction/inmate_info/", executions[i], sep=""), fields = NULL, encoding = "UTF-8")
 		lastStatement = readLines(paste("../data_extraction/inmate_last_statement/", downloadedExecution$executionNumber, sep=""))
@@ -43,6 +43,7 @@ prepareInmateDocument <- function() {
 		# remove beggining and ending whitespace
 		lastStatement = rapportools::trim.space(lastStatement)
 
+		inmate_execution_number[i] = downloadedExecution$executionNumber
 		inmate_first_names[i] = downloadedExecution$firstName
 		inmate_last_names[i] = downloadedExecution$lastName
 		inmate_execution_dates[i] = downloadedExecution$executionDate
@@ -62,14 +63,15 @@ prepareInmateDocument <- function() {
 
 	# prepare a dataframe
 	Inmates = data.frame(
+		execution_number = inmate_execution_number,
 		first_name = inmate_first_names,
 		last_name = inmate_last_names,
 		execution_date = inmate_execution_dates,
 		race = inmate_races,
-		dob = inmate_dates_of_birth,
+		date_of_birth = inmate_dates_of_birth,
 		date_received = inmate_dates_received,
 		eye_color = inmate_eye_colors,
-		doo = inmate_dates_of_offense,
+		date_of_offense = inmate_dates_of_offense,
 		gender = inmate_genders,
 		hair_color = inmate_hair_colors,
 		native_county = inmate_native_counties,
@@ -78,27 +80,22 @@ prepareInmateDocument <- function() {
 		occupation = inmate_occupations,
 		last_statement = inmate_last_statements)
 
-	# sort by execution_number
-	Inmates <- Inmates[order(-Inmates$execution_number),]
-
-	comment(Inmates$execution_number)<-c("Execution number for the inmate. The first inmate to be executed is labeled as 1.")
-	comment(Inmates$tdjc_number)<-c("Texas department of justice number")
-	comment(Inmates$first_name)<-c("Inmate's first name.")
-	comment(Inmates$last_name)<-c("Inmate's last name.")
-	comment(Inmates$gender)<-c("Inmate's gender")
-	comment(Inmates$eye_color)<-c("Inmate's eye color")
-	comment(Inmates$hair_color)<-c("Inmate's hair color")
-	comment(Inmates$education_level)<-c("Inmate's education level. n/a means unavailable")
-	comment(Inmates$date_of_offense)<-c("The date of when the final crime was commited")
-	comment(Inmates$age_at_execution)<-c("Inmate's age when executed")
+	comment(Inmates$execution_number)<-c("Execution number for the inmate. The first inmate to be executed is labeled as 1")
+	comment(Inmates$first_name)<-c("Inmate's first name")
+	comment(Inmates$last_name)<-c("Inmate's last name")
 	comment(Inmates$execution_date)<-c("Date of execution")	
 	comment(Inmates$race)<-c("Inmate's race")
-	comment(Inmates$county)<-c("is this the county where the crime was commited?")
-	comment(Inmates$native_county)<-c("is this the county where the criminal grew up?")
-	comment(Inmates$native_state)<-c("is this the state where the criminal grew up?")
-	comment(Inmates$last_statement)<-c("Last statement text that was automatically extracted from the inmate's webpage.")
-	comment(Inmates$info_link)<-c("This is the link you can use to get more details on the inmate.")
-	comment(Inmates$last_statement_link)<-c("This is the link you can use to get the last statement for the inmate.")
-
+	comment(Inmates$date_of_birth)<-c("Inmate's date of birth")
+	comment(Inmates$date_received)<-c("Date when inmate went to deathrow prison, or whatever that place is called.")
+	comment(Inmates$eye_color)<-c("Inmate's eye color")
+	comment(Inmates$date_of_offense)<-c("The date of when the final crime was commited")
+	comment(Inmates$gender)<-c("Inmate's gender")
+	comment(Inmates$hair_color)<-c("Inmate's hair color")
+	comment(Inmates$native_county)<-c("I think this is the county where the criminal grew up")
+	comment(Inmates$native_state)<-c("I think this the state where the criminal grew up")
+	comment(Inmates$education_level)<-c("Inmate's education level. It is measured in years, which is odd. e.g 16 years means 12 years of highschool and 4 years of college")
+	comment(Inmates$occupation)<-c("Professional occupation of the inmate")
+	comment(Inmates$last_statement)<-c("Last statement that was issued by the inmate")
+	print(paste("inmate nodel built! 🥳 Run str(Inmates) to get started"))
 	return(Inmates)
 }
