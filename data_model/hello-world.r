3025ce735479215ca84fb6c030a727da1ca78a9d
Slobodan Milanko <support@justprogress.net>
2022-03-30 11:07:23 -0400

    updates to the hello world

--- data_model/hello-world.r
+++ data_model/hello-world.r
@@ -1,20 +1,30 @@
-library(dplyr)
-
-# this file will generate the inmate documents
+library("dplyr")
+library("rapport")
 source("prepareInmateDocument.r")
 
-# load the inmage
+showDataLevels <- function(Inmates) {
+	print(levels(factor(Inmates$gender)))
+	print(levels(factor(Inmates$eye_color)))
+	print(levels(factor(Inmates$hair_color)))
+	print(levels(factor(Inmates$race)))
+	print(levels(factor(Inmates$native_county)))
+	print(levels(factor(Inmates$native_state)))
+	print(levels(factor(Inmates$education_level)))
+
+	allOccupations = Inmates$occupation
+	unique_occupations = c()
+	for (i in 1:length(allOccupations)) {
+		# split the occupation into parts, since individuals can have more than one profession
+		currentOccupations = unlist(str_split(Inmates$occupation[i], ","))
+		for (j in 1:length(currentOccupations)) { 
+			unique_occupations = append(unique_occupations, rapportools::trim.space(currentOccupations[j])) 
+		}
+	}
+
+	unique_occupations = unique_occupations[!duplicated(unique_occupations)]
+	levels(factor(unique_occupations))
+}
+
+# load the inmates
 Inmates = prepareInmateDocument()
-
-# data checks
-levels(factor(Inmates$gender))
-levels(factor(Inmates$eye_color))
-levels(factor(Inmates$hair_color))
-levels(factor(Inmates$race))
-levels(factor(Inmates$native_county))
-levels(factor(Inmates$native_state))
-levels(factor(Inmates$education_level))
-
-# dan is here, making more checks
-
-levels(factor(Inmates$occupation))
+showDataLevels(Inmates)
