15be67d3e332c459125885aed4cacabcf6357ddb
Slobodan Milanko <support@justprogress.net>
2022-03-29 23:54:42 -0400

    added some basic checks to the hello world

--- data_model/hello-world.r
+++ data_model/hello-world.r
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # this file will generate the inmate documents
 source("prepareInmateDocument.r")
 
@@ -5,3 +7,6 @@ source("prepareInmateDocument.r")
 Inmates = prepareInmateDocument()
 
 # do something fun
+levels(factor(Inmates$gender))
+levels(factor(Inmates$eye_color))
+levels(factor(Inmates$hair_color))
