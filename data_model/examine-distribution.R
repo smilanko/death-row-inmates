a86338131d66bb9e59bf5a8120b5a5f5f4401e1a
Slobodan Milanko <support@justprogress.net>
2022-04-01 19:51:50 -0400

    changes to the examine distro script

--- data_model/examine-distribution.R
+++ data_model/examine-distribution.R
@@ -1,7 +1,11 @@
 library("dplyr")
 library("rapport")
 library("tm")
+library("data.table") 
 source("prepareInmateDocument.r")
+
+# set the seed for reproducability
+set.seed(3)
 
 # load the inmates
 Inmates = prepareInmateDocument()
@@ -13,25 +17,34 @@ documents = tm_map(documents, removePunctuation)
 documents = tm_map(documents, removeWords, stopwords("english"))
 documents <- tm_map(documents, stripWhitespace)
 
-# set the seed for reproducability
-set.seed(3)
-
 # create a document term matrix
 dtm <- DocumentTermMatrix(documents)
 rowTotals <- apply(dtm , 1, sum) # Find the sum of words in each Document
-dtm <- dtm[rowTotals> 2, ] # get documents with at least 2 ter,s
-spokenWords = rowSums(as.matrix(dtm))
+document_count_filter = rowTotals> 2 # get documents with at least 2 words
+dtm <- dtm[document_count_filter, ] 
+spokenWords = as.data.frame(log(rowSums(as.matrix(dtm))))[,1]
 
 # how long is the inmate in jail
 days_in_jail = difftime(as.Date(Inmates$date_received, format = "%m/%d/%Y"), as.Date(Inmates$execution_date, format = "%m/%d/%Y"), units = "days")
-hist(as.numeric(days_in_jail), prob = TRUE)
+# apply the same filter to remove inmates with little words
+days_in_jail = days_in_jail[document_count_filter]
 
+# we do not want data that has no days in fail over over 19 years
+jail_sentance_filter = which(is.na(days_in_jail) | (days_in_jail < -7000))
+spokenWords = spokenWords[-jail_sentance_filter]
+# qq norm plot of spokenWords
+par(mfrow=c(2,2))
+qqnorm(spokenWords, pch = 1, frame = FALSE)
+qqline(spokenWords, col = "steelblue", lwd = 2)
+hist(spokenWords, prob = TRUE)
+lines(density(spokenWords), col = 4, lwd = 2)
 
-# qq norm plot
-qqnorm(log(spokenWords), pch = 1, frame = FALSE)
-qqline(log(spokenWords), col = "steelblue", lwd = 2)
+days_in_jail = as.numeric(days_in_jail[-jail_sentance_filter])
+qqnorm(days_in_jail, pch = 1, frame = FALSE)
+qqline(days_in_jail, col = "steelblue", lwd = 2)
+hist(days_in_jail, prob = TRUE)
+lines(density(days_in_jail), col = 4, lwd = 2)
 
-# Log transform of the number of words in the last statement
-hist(log(spokenWords), prob = TRUE)
-lines(density(log(spokenWords)), col = 4, lwd = 2)
-
+m1 = lm(spokenWords~days_in_jail)
+par(mfrow=c(1,4))
+plot(m1)
