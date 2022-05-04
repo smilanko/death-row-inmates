71bd2c1f4a61f5de7b368cc05916e0197afbe83f
JamillaPH <102705129+JamillaPH@users.noreply.github.com>
2022-05-03 23:07:03 -0400

    Add files via upload

---
+++ data_model/jamillaphosein-forgiveness.R
@@ -0,0 +1,72 @@
+library("properties")
+library("rvest")
+library("stringr")
+library("rapport")
+library("dplyr")
+library("utils")
+library("DescTools")
+library("tm")
+library("psych")
+library("sjmisc")
+library("grDevices")
+
+setwd("~/Downloads/death-row-inmates-main 2/data_model")
+
+source("prepareInmateDocument.r")
+
+Inmates = prepareInmateDocument()
+
+#silence the warning, since this script is verified
+options(warn=-1)
+
+#set seed 
+set.seed(9)
+
+#filter out empty statements
+empty_last_statement_filter = which(Inmates$last_statement == "")
+Inmates <- Inmates[-empty_last_statement_filter, ]
+#450 observations left  
+
+#create factor variables for forgiveness: 0 = does not say "forgive" 1 = does say "forgive"
+Inmates$has_forgive <- ifelse(grepl("forgive", Inmates$last_statement, ignore.case = TRUE), "1","0")
+View(Inmates)
+forgive <- Inmates$has_forgive
+
+#checking how many observations for each variable
+yes_forgive <- str_subset(forgive, "1")
+yes_forgive.data <- data.frame(yes_forgive)
+#115 observations
+no_forgive <- str_subset(forgive, "0")
+no_forgive.data <- data.frame(no_forgive)
+#302 observations
+
+
+#calculate days in jail
+days_in_jail = difftime(as.Date(Inmates$date_received, format = "%m/%d/%Y"), as.Date(Inmates$execution_date, format = "%m/%d/%Y"), units = "days")
+
+#we do not want data that has no days in jail over 19 years
+jail_sentence_filter <- which(is.na(days_in_jail) | (days_in_jail < -7000))
+Inmates <- Inmates[-jail_sentence_filter]
+days_in_jail <- as.numeric(days_in_jail[-jail_sentence_filter])
+forgive <- forgive[-jail_sentence_filter]
+days_in_jail
+forgive
+
+
+m1 = lm(forgive~days_in_jail)
+
+
+setEPS()
+postscript("~/Downloads/death-row-inmates-main 2/data_model/plots/forgiveness_plots/forgive_linear.eps",width=12.5,height=4)
+par(mfrow=c(1,4))
+plot(m1)
+dev.off()
+print(summary(m1))
+
+#correlation plot
+setEPS()
+postscript("~/Downloads/death-row-inmates-main 2/data_model/plots/forgiveness_plots/forgive_correlation.eps",width=12.5,height=4)
+pairs.panels(data.frame(days_in_jail, forgive), labels =c('Days on Death Row', 'Presence of Forgive in Last Statement'), smooth = TRUE, scale = FALSE, density = TRUE, ellipses = TRUE,  method = "pearson",  pch = 21, lm = FALSE, cor = TRUE, jiggle = FALSE, factor = 2,  hist.col = 4, stars = TRUE,  ci = TRUE)
+
+
+
